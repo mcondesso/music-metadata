708d084c696249911a23735642af1381faa15043
Miguel Condesso <mcondesso@mail.com>
2023-12-07 22:11:10 +0100

    Added support for .mp4 audio files

--- main.py
+++ main.py
@@ -3,33 +3,59 @@ from typing import Tuple
 
 from tqdm import tqdm
 from mutagen.easyid3 import EasyID3
+from mutagen.mp4 import MP4
 from dotenv import load_dotenv
 
 load_dotenv(".env")
 
+SUPPORTED_FORMATS = {".mp3", ".mp4"}
 
-def get_mp3_files(directory: str) -> Tuple[str, str]:
-    for root, dirs, files in os.walk(directory):
+METADATA_TAGS = {
+    ".mp3": {
+        "artist": "artist",
+        "title": "title",
+    },
+    ".mp4": {
+        "artist": "©ART",
+        "title": "©nam",
+    },
+}
+
+METADATA_CLASSES = {
+    ".mp3": EasyID3,
+    ".mp4": MP4,
+}
+
+
+def get_music_files(directory: str) -> Tuple[str, str]:
+    for root, _, files in os.walk(directory):
         for filename in files:
-            yield (root, filename)
+            if os.path.splitext(filename)[1] in SUPPORTED_FORMATS:
+                yield (root, filename)
 
 
 def parse_filename(filename: str) -> dict:
-    without_extension = os.path.splitext(filename)[0]
-    without_extension = without_extension.replace("–", "-")
+    filename_no_extension, file_extension = os.path.splitext(filename)
+    artist_tag = METADATA_TAGS[file_extension]["artist"]
+    title_tag = METADATA_TAGS[file_extension]["title"]
+
+    filename_no_extension = filename_no_extension.replace("–", "-")
     try:
-        artists, title = without_extension.split(" - ", maxsplit=1)
+        artists, title = filename_no_extension.split(" - ", maxsplit=1)
     except ValueError as error:
-        raise ValueError(f'error parsing filename "{filename}"')
+        raise ValueError(f'error parsing filename "{filename}"') from error
     else:
         return {
-            "artist": artists.split("&"),
-            "title": title,
+            artist_tag: artists.split("&"),
+            title_tag: title,
         }
 
 
 def set_file_tags(filepath: str, tags: dict):
-    tag_dict = EasyID3(filepath)
+    file_extension = os.path.splitext(filepath)[1]
+    tag_handling_class = METADATA_CLASSES[file_extension]
+
+    tag_dict = tag_handling_class(filepath)
     for key, value in tags.items():
         tag_dict[key] = value
     tag_dict.save()
@@ -37,7 +63,7 @@ def set_file_tags(filepath: str, tags: dict):
 
 if __name__ == "__main__":
     collection_path = os.getenv("COLLECTION_PATH")
-    for folder, filename in tqdm(get_mp3_files(collection_path)):
+    for folder, filename in tqdm(get_music_files(collection_path)):
         metadata = parse_filename(filename)
         set_file_tags(
             filepath=os.path.join(folder, filename),
